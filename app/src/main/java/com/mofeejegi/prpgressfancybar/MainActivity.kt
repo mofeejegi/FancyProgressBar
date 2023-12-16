468fc0b08b5f2212560e891f9ea32f4b7cb7c68e
Mofe Ejegi <mofeejegi@gmail.com>
2023-12-16 13:03:00 +0100

    Update to Android/Kotlin 2023 standards

--- app/src/main/java/com/mofeejegi/prpgressfancybar/MainActivity.kt
+++ app/src/main/java/com/mofeejegi/prpgressfancybar/MainActivity.kt
@@ -3,14 +3,18 @@ package com.mofeejegi.prpgressfancybar
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val progress = findViewById<RoundedProgress>(R.id.progress)
+        val digitText1 = findViewById<DigitTextView>(R.id.digitText1)
+        val digitText2 = findViewById<DigitTextView>(R.id.digitText2)
+        val digitText3 = findViewById<DigitTextView>(R.id.digitText3)
 
         progress.setOnClickListener{
             val score = Random().nextInt(601) + 300f
@@ -22,9 +26,6 @@ class MainActivity : AppCompatActivity() {
             digitText1.setValue(score.toString().substring(0, 1).toInt(), true, false)
             digitText2.setValue(score.toString().substring(1, 2).toInt(), true, true)
             digitText3.setValue(score.toString().substring(2, 3).toInt(), true, false)
-//            digitText1.setValue(9, true, false)
-//            digitText2.setValue(9, true, true)
-//            digitText3.setValue(9, true, false)
         }
     }
 }
